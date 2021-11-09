180d6890856c339ad114c70d064aa6908d3d7159
Diana001a <82045854+Diana001a@users.noreply.github.com>
2021-11-09 19:26:35 +0300

    weqwr
    rtfaqewt

---
+++ Kisik/Kisik.DataAccess.Core/Models/VirtualExpenseRto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Kisik.DataAccess.Core.Enums;
+
+namespace Kisik.DataAccess.Core.Models
+{
+    [Table("VirtualExpense")]
+    class VirtualExpenseRto
+    {
+        [Key] public int Id { get; set; }
+        public float Cost { get; set; }
+        public CostCurrencyRto CostCurrency { get; set; }
+        public VirtualExpenseStatusRto Status { get; set; }
+        public DateTimeOffset CreateDateTime { get; set; }
+        public List<DateTimeOffset> ChangeDateTimes{ get; set; }
+    }
+}
